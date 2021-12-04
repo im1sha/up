5aae3e5c9ebd3381ad0c6f534b6feb25cb150f22
Mikhail Ovchelupov <mikefoxx2000@ya.ru>
2021-12-04 12:02:50 +0300

    Added a simple animation

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+const dx = 2;
+const dy = -2;
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+setInterval(draw, 1);
 
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.beginPath();
+
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#FF0000";
+    ctx.fill();
+    
+    ctx.closePath();
+    
+    x += dx;
+    y += dy;
+}
