45d00ee63ef91ea629250917c82b11f31210fff2
Mikhail Ovchelupov <mikefoxx2000@ya.ru>
2021-12-04 12:23:37 +0300

    Added border exit handling

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 var x = canvas.width / 2;
 var y = canvas.height - 30;
-const dx = 2;
-const dy = -2;
+var dx = 2;
+var dy = -2;
+const ballRadius = 10;
 
 setInterval(draw, 1);
 
@@ -12,12 +13,19 @@ function draw() {
 
     ctx.beginPath();
 
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "#FF0000";
     ctx.fill();
     
     ctx.closePath();
-    
+
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+
     x += dx;
     y += dy;
 }
