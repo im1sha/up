150859d4e7675fd5376301c8037e371f33fe2535
Mikhail Ovchelupov <mikefoxx2000@ya.ru>
2021-12-24 12:59:10 +0300

    Reordered script content

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
-
 class Ball {
     constructor(canvas) {
         {
@@ -211,33 +208,81 @@ class Paddle extends Brick {
     get dx() { return 7; }
 }
 
-const paddle = new Paddle(
-    ctx,
-    (canvas.width - Paddle.defaultWidth) / 2,
-    canvas.height - Paddle.defaultHeight,
-    Paddle.defaultWidth,
-    Paddle.defaultHeight,
-    canvas);
+class CollisionPosition {
+    static get left() { return 'left'; }
+    static get right() { return 'right'; }
+    static get top() { return 'top'; }
+    static get bottom() { return 'bottom'; }
+}
 
-const brickCount = { 'row': 3, 'column': 5, };
-const brickOffset = { 'top' : 30, 'left' : 30, }; 
-let bricks = Array.from(
-    { length: brickCount.column },
-    (_, c) => Array.from(
-        { length: brickCount.row },
-        (_, r) => new Brick(
-            ctx,
-            (c * (Brick.defaultWidth + Brick.defaultPadding)) + brickOffset.left,
-            (r * (Brick.defaultHeight + Brick.defaultPadding)) + brickOffset.top,
-            Brick.defaultWidth,
-            Brick.defaultHeight)))
-    .flat();
+class CollisionCandidate {
+    constructor(position,
+                radius, 
+                axis1Center, 
+                axis2Center, 
+                axis2Coordinate, 
+                axis1Min, 
+                axis1Max,
+                object) {
+        {
+            if (position === undefined || position === null) throw new Error(); 
+            if (radius === undefined || radius === null) throw new Error(); 
+            if (axis1Center === undefined || axis1Center === null) throw new Error(); 
+            if (axis2Center === undefined || axis2Center === null) throw new Error(); 
+            if (axis2Coordinate === undefined || axis2Coordinate === null) throw new Error(); 
+            if (axis1Min === undefined || axis1Min === null) throw new Error(); 
+            if (axis1Max === undefined || axis1Max === null) throw new Error(); 
+            if (object === undefined || object === null) throw new Error(); 
+        }
+        this.position = position;
+        this.radius = radius;
+        this.axis1Center = axis1Center;
+        this.axis2Center = axis2Center;
+        this.axis2Coordinate = axis2Coordinate;
+        this.axis1Min = axis1Min;
+        this.axis1Max = axis1Max;
+        this.object = object;
+    }
 
-let ball = new Ball(canvas);
-let outerBound = new OuterBound(ball, canvas);
+    toString() {
+        return 'position: ' + this.position +
+            ', r: ' + this.radius +
+            ', a1c: ' + this.axis1Center +
+            ', a2c: ' + this.axis2Center +
+            ', a2: ' + this.axis2Coordinate +
+            ', a1min: ' + this.axis1Min +
+            ', a1max: ' + this.axis1Max; 
+    }
+}
 
-let score = 0;
-let lives = 3;
+function intersects(circle, rect) {  
+    const cases = [
+        new CollisionCandidate(CollisionPosition.left, circle.radius, circle.y, circle.x, rect.x, rect.y, rect.y + rect.height, rect),
+        new CollisionCandidate(CollisionPosition.right, circle.radius, circle.y, circle.x, rect.x + rect.width, rect.y, rect.y + rect.height, rect),
+        new CollisionCandidate(CollisionPosition.top, circle.radius, circle.x, circle.y, rect.y, rect.x, rect.x + rect.width, rect),
+        new CollisionCandidate(CollisionPosition.bottom, circle.radius, circle.x, circle.y, rect.y + rect.height, rect.x, rect.x + rect.width, rect),
+    ];
+    
+    return cases.filter((c, i, _) => { 
+        const intersection = getIntersection(c.radius, c.axis1Center, c.axis2Center, c.axis2Coordinate);
+        
+        const result = intersection === null
+            ? false
+            : intersection.some((v, j, _) => v >= c.axis1Min && v <= c.axis1Max);
+
+        if (result === true)
+            console.log(c.toString());
+
+        return result;
+    });
+
+    // intersection of a line (asix2 = asix2Coordinate) with a circle(r, asix1Center, asix2Center)
+    function getIntersection(r, asix1Center, asix2Center, asix2Coordinate) {
+        const c = r ** 2 - (asix2Coordinate - asix2Center) ** 2;
+        if (c < 0) return null;
+        return [Math.sqrt(c) + asix1Center, -Math.sqrt(c) + asix1Center];
+    }
+}
 
 function main() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -311,80 +356,35 @@ function displayLives(ctx, width, lives) {
     ctx.fillText("Lives: " + lives, width - 65, 20);
 }
 
-function intersects(circle, rect) {  
-    const cases = [
-        new CollisionCandidate(CollisionPosition.left, circle.radius, circle.y, circle.x, rect.x, rect.y, rect.y + rect.height, rect),
-        new CollisionCandidate(CollisionPosition.right, circle.radius, circle.y, circle.x, rect.x + rect.width, rect.y, rect.y + rect.height, rect),
-        new CollisionCandidate(CollisionPosition.top, circle.radius, circle.x, circle.y, rect.y, rect.x, rect.x + rect.width, rect),
-        new CollisionCandidate(CollisionPosition.bottom, circle.radius, circle.x, circle.y, rect.y + rect.height, rect.x, rect.x + rect.width, rect),
-    ];
-    
-    return cases.filter((c, i, _) => { 
-        const intersection = getIntersection(c.radius, c.axis1Center, c.axis2Center, c.axis2Coordinate);
-        
-        const result = intersection === null
-            ? false
-            : intersection.some((v, j, _) => v >= c.axis1Min && v <= c.axis1Max);
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
 
-        if (result === true)
-            console.log(c.toString());
+const paddle = new Paddle(
+    ctx,
+    (canvas.width - Paddle.defaultWidth) / 2,
+    canvas.height - Paddle.defaultHeight,
+    Paddle.defaultWidth,
+    Paddle.defaultHeight,
+    canvas);
 
-        return result;
-    });
+const brickCount = { 'row': 3, 'column': 5, };
+const brickOffset = { 'top' : 30, 'left' : 30, }; 
+let bricks = Array.from(
+    { length: brickCount.column },
+    (_, c) => Array.from(
+        { length: brickCount.row },
+        (_, r) => new Brick(
+            ctx,
+            (c * (Brick.defaultWidth + Brick.defaultPadding)) + brickOffset.left,
+            (r * (Brick.defaultHeight + Brick.defaultPadding)) + brickOffset.top,
+            Brick.defaultWidth,
+            Brick.defaultHeight)))
+    .flat();
 
-    // intersection of a line (asix2 = asix2Coordinate) with a circle(r, asix1Center, asix2Center)
-    function getIntersection(r, asix1Center, asix2Center, asix2Coordinate) {
-        const c = r ** 2 - (asix2Coordinate - asix2Center) ** 2;
-        if (c < 0) return null;
-        return [Math.sqrt(c) + asix1Center, -Math.sqrt(c) + asix1Center];
-    }
-}
+let ball = new Ball(canvas);
+let outerBound = new OuterBound(ball, canvas);
 
-class CollisionPosition {
-    static get left() { return 'left'; }
-    static get right() { return 'right'; }
-    static get top() { return 'top'; }
-    static get bottom() { return 'bottom'; }
-}
-
-class CollisionCandidate {
-    constructor(position,
-                radius, 
-                axis1Center, 
-                axis2Center, 
-                axis2Coordinate, 
-                axis1Min, 
-                axis1Max,
-                object) {
-        {
-            if (position === undefined || position === null) throw new Error(); 
-            if (radius === undefined || radius === null) throw new Error(); 
-            if (axis1Center === undefined || axis1Center === null) throw new Error(); 
-            if (axis2Center === undefined || axis2Center === null) throw new Error(); 
-            if (axis2Coordinate === undefined || axis2Coordinate === null) throw new Error(); 
-            if (axis1Min === undefined || axis1Min === null) throw new Error(); 
-            if (axis1Max === undefined || axis1Max === null) throw new Error(); 
-            if (object === undefined || object === null) throw new Error(); 
-        }
-        this.position = position;
-        this.radius = radius;
-        this.axis1Center = axis1Center;
-        this.axis2Center = axis2Center;
-        this.axis2Coordinate = axis2Coordinate;
-        this.axis1Min = axis1Min;
-        this.axis1Max = axis1Max;
-        this.object = object;
-    }
-
-    toString() {
-        return 'position: ' + this.position +
-            ', r: ' + this.radius +
-            ', a1c: ' + this.axis1Center +
-            ', a2c: ' + this.axis2Center +
-            ', a2: ' + this.axis2Coordinate +
-            ', a1min: ' + this.axis1Min +
-            ', a1max: ' + this.axis1Max; 
-    }
-}
+let score = 0;
+let lives = 3;
 
 main();
